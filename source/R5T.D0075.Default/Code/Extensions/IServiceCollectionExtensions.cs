723e5e449975a11700b97febd692ba3b335b0e3b
David Coats <david@getsafetycone.com>
2021-06-02 12:07:51 -0700

    Add Base service definition library and Default service implementation library. Remove extraneous R5T.D0075 library. Add ICommandLineOperator service definition and implementation.

---
+++ source/R5T.D0075.Default/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Dacia;
+
+
+namespace R5T.E0024.D001
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="CommandLineOperator"/> implementation of <see cref="ICommandLineOperator"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddCommandLineOperator(this IServiceCollection services)
+        {
+            services.AddSingleton<ICommandLineOperator, CommandLineOperator>();
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="CommandLineOperator"/> implementation of <see cref="ICommandLineOperator"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceAction<ICommandLineOperator> AddCommandLineOperatorAction(this IServiceCollection services)
+        {
+            var serviceAction = ServiceAction.New<ICommandLineOperator>(() => services.AddCommandLineOperator());
+            return serviceAction;
+        }
+    }
+}
---
+++ source/R5T.D0075.Default/Code/Services/Implementations/CommandLineOperator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using R5T.T0030;
+
+
+namespace R5T.E0024.D001
+{
+    public class CommandLineOperator : ICommandLineOperator
+    {
+        public Task<int> Run(string command, string arguments, DataReceivedEventHandler receiveOutputData, DataReceivedEventHandler receiveErrorData)
+        {
+            return CommandLine.Run(command, arguments, receiveOutputData, receiveErrorData);
+        }
+    }
+}
